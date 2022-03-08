e3a22a283c4608c94cd793d6dbffd511b14ecae1
Monodytheone <monody_tfr@qq.com>
2022-03-08 19:44:44 +0800

    Revert "改注释"
    This reverts commit 1cb84e44e6a319275f852884cc9765e79d794b04.

--- CardGame/Assets/Scripts/SaveDeck.cs
+++ CardGame/Assets/Scripts/SaveDeck.cs
@@ -1,16 +1,16 @@
-ï»¿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// <para>ç‚¹å‡»SaveDeckæŒ‰é’®å°±æŠŠå¡ç»„ä¿å­˜åˆ°PlayerData.csv</para>
-/// <para>æŒ‚åœ¨DataManagerä¸Š</para>
-/// <para>ç„¶åè¦åœ¨SaveDeckè¿™ä¸ªæŒ‰é’®çš„Inspectoré¡µé¢ä¸ŠæŠŠOn Click()ç»‘åˆ°è¯¥è„šæœ¬çš„OnClickSaveDeck()æ–¹æ³•</para>
+/// <para>µã»÷SaveDeck°´Å¥¾Í°Ñ¿¨×é±£´æµ½PlayerData.csv</para>
+/// <para>¹ÒÔÚDataManagerÉÏ</para>
+/// <para>È»ºóÒªÔÚSaveDeckÕâ¸ö°´Å¥µÄInspectorÒ³ÃæÉÏ°ÑOn Click()°óµ½¸Ã½Å±¾µÄOnClickSaveDeck()·½·¨</para>
 /// </summary>
 public class SaveDeck : MonoBehaviour
 {
     /// <summary>
-    /// <para>ç‚¹å‡»SaveDeckæŒ‰é’®å°±æŠŠå¡ç»„ä¿å­˜åˆ°PlayerData.csv</para>
+    /// <para>µã»÷SaveDeck°´Å¥¾Í°Ñ¿¨×é±£´æµ½PlayerData.csv</para>
     /// </summary>
     public void OnClickSaveDeck()
     {
