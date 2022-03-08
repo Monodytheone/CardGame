b5437ed543a28d784cc9f3168feacc68cfb36889
Monodytheone <monody_tfr@qq.com>
2022-03-08 21:09:40 +0800

    Update SaveDeck.cs

--- CardGame/Assets/Scripts/SaveDeck.cs
+++ CardGame/Assets/Scripts/SaveDeck.cs
@@ -1,17 +1,1 @@
-<
-using System.Collections;
-=======
-﻿using System.Collections;
->>>>>>> parent of bb45b15 (简化SaveDeck)
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SaveDeck : MonoBehaviour
-{
-    public void OnClickSaveDeck()
-    {
-        GetComponent<PlayerData>().SavePlayerData();
-    }
-}
+﻿
