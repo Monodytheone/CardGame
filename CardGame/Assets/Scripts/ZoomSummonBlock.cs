b79b202cd320a31d8bd3fbf84402905b818d9702
Monodytheone <monody_tfr@qq.com>
2022-03-13 16:29:20 +0800

    怪物卡召唤基本流程已实现
    1. 召唤时机判定
    2. 可召唤格子会高亮，鼠标移入会放大
    3. 点击Trun End按钮时使所有格子退出高亮状态；如果点击手牌之后不想召唤了可以之间点Trun End结束回合
    
    run
    
    run
    
    Summon Builing_1
    
    Summon Building _2
    
    开发箭头之前
    
    注释掉原本的TurnEnd()

---
+++ CardGame/Assets/Scripts/ZoomSummonBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ZoomSummonBlock : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(gameObject.GetComponent<Block>().SummonBlock.activeInHierarchy)  // 如果是高亮
+        {
+            transform.localScale = new Vector3(1.2f, 1.2f, 1.0f);  // x, y都扩大到原来的1.2倍
+        }
+    }
+
+    /// <summary>
+    /// 这个方法即使格子原本没有高亮也会执行，以后要找办法，让只有鼠标已经进入之后，才能触发该方法的判定
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("鼠标离开Block");
+        transform.localScale = Vector3.one;
+    }    
+}
