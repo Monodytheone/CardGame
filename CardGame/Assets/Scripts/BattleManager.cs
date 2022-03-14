a632f08ee547472ff82f1c437a0b59451b862bb9
Monodytheone <monody_tfr@qq.com>
2022-03-14 13:18:52 +0800

    开发箭头之前
    注释掉原本的TurnEnd()

--- CardGame/Assets/Scripts/BattleManager.cs
+++ CardGame/Assets/Scripts/BattleManager.cs
@@ -168,14 +168,16 @@ public class BattleManager : MonoSingleton<BattleManager>  // 直接继承单例
         if (GamePhase == GamePhase.playerDraw)
         {
             DrawCard(0, 1);
-            GamePhase = GamePhase.playerAction;
-            phaseChangeEvent.Invoke();
+            //GamePhase = GamePhase.playerAction;
+            //phaseChangeEvent.Invoke();
+            NextPhase();
         }
         else if (GamePhase == GamePhase.enemyDraw)
         {
             DrawCard(1, 1);
-            GamePhase = GamePhase.enemyAction;
-            phaseChangeEvent.Invoke();
+            //GamePhase = GamePhase.enemyAction;
+            //phaseChangeEvent.Invoke();
+            NextPhase();
         }
         else
         {
@@ -314,27 +316,149 @@ public class BattleManager : MonoSingleton<BattleManager>  // 直接继承单例
     /// <summary>
     /// 用于玩家或敌人回合结束时切换回合
     /// </summary>
-    public void TurnEnd()
+
+    //public void TurnEnd()
+    //{
+    //    // 若玩家行动回合结束，则跳转到敌人抽卡
+    //    if(GamePhase == GamePhase.playerAction)
+    //    {
+    //        GamePhase = GamePhase.enemyDraw;
+    //        Debug.Log("TurnEnd()");
+    //    }
+    //    else if(GamePhase == GamePhase.enemyAction)  // 若敌人行动回合结束，则跳转到玩家抽卡阶段
+    //    {
+    //        GamePhase = GamePhase.playerDraw;
+    //        Debug.Log("TurnEnd()");
+    //    }
+    //    else if(GamePhase == GamePhase.playerDraw)
+    //    {
+    //        GamePhase = GamePhase.playerAction;
+    //    }
+    //    else if(GamePhase == GamePhase.enemyDraw)
+    //    {
+    //        GamePhase = GamePhase.enemyAction;
+    //    }
+    //    phaseChangeEvent.Invoke();
+    //}
+
+    /// <summary>
+    /// 用于进入下一阶段
+    /// </summary>
+    private void NextPhase()
     {
-        // 若玩家行动回合结束，则跳转到敌人抽卡
-        if(GamePhase == GamePhase.playerAction)
-        {
-            GamePhase = GamePhase.enemyDraw;
-            Debug.Log("TurnEnd()");
-        }
-        else if(GamePhase == GamePhase.enemyAction)  // 若敌人行动回合结束，则跳转到玩家抽卡阶段
+        Debug.Log("PhaseChange()");
+        //if (GamePhase == GamePhase.enemyAction)
+        if((int)GamePhase == System.Enum.GetNames(GamePhase.GetType()).Length - 1)
         {
             GamePhase = GamePhase.playerDraw;
-            Debug.Log("TurnEnd()");
         }
-        else if(GamePhase == GamePhase.playerDraw)
+        else
+            GamePhase++;  // 进入下一阶段   
+
+        // 关闭所有格子的高亮
+        foreach (var block in playerBlocks)
         {
-            GamePhase = GamePhase.playerAction;
+            block.GetComponent<Block>().SummonBlock.SetActive(false);
         }
-        else if(GamePhase == GamePhase.enemyDraw)
+        foreach (var block in enemyBlocks)
         {
-            GamePhase = GamePhase.enemyAction;
+            block.GetComponent<Block>().SummonBlock.SetActive(false);
         }
+
         phaseChangeEvent.Invoke();
+    }
+
+
+
+
+    /// <summary>
+    /// 召唤请求
+    /// <para>用于卡牌被点击时，发起召唤请求，开始等待点击格子</para>
+    /// </summary>
+    /// <param name="_player">0表示玩家，1表示敌人</param>
+    /// <param name="_monster">要召唤的怪兽卡</param>
+    public void SummonRequest(int _player, GameObject _monster)  // 疑问：不需要返回一下召唤请求是否成功嘛？
+    {
+        GameObject[] blocks = new GameObject[1];
+        bool hasEmptyBlock = false;  // 是否具有可召唤到的空格子
+        if(_player == 0 && GamePhase == GamePhase.playerAction)
+        {
+            blocks = playerBlocks;
+        }
+        else if (_player == 1 && GamePhase == GamePhase.enemyAction)
+        {
+            blocks = enemyBlocks;
+        }
+        else
+        {
+            Debug.Log("不可召唤");
+            return;
+        }
+        if (SummonCounter[_player] > 0)  // 还有剩余召唤次数时，才可召唤
+        {
+            foreach (var block in blocks)  // 还有空格子时，才可召唤
+            {
+                if (block.GetComponent<Block>().card == null)
+                {
+                    Debug.Log("空格子");
+                    // 还要对所有等待召唤（空）的格子进行高亮显示
+                    block.GetComponent<Block>().SummonBlock.SetActive(true);  // 高亮显示
+
+                    hasEmptyBlock = true;
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("可召唤次数为0");
+        }
+        if (hasEmptyBlock)
+        {
+            waitingMonster = _monster;
+            waitingPlayer = _player;
+            Debug.Log("召唤请求通过");
+        }
+    }
+
+    /// <summary>
+    /// 召唤确认
+    /// <para>用于格子被点击时，确认召唤，准备执行召唤</para>
+    /// <para>为何不能点击格子后直接执行召唤呢？</para>
+    /// <para>   答：若敌人为Ai控制，自然无法点击格子，需要让Ai直接能调用Summon进行召唤</para>
+    /// </summary>
+    /// <param name="_block">要召唤到的格子</param>
+    public void SummonConfirm(Transform _block)
+    {
+        // 具体的召唤确认在Block里实现
+        Summon(waitingPlayer, waitingMonster, _block);  // 执行召唤
+
+        // 执行召唤之后要关闭格子的高亮，由于格子只有在格子高亮时才可召唤，故关闭高亮后格子就不可作为召唤目标位置了
+        GameObject[] blocks = playerBlocks;
+        if(waitingPlayer == 1)
+        {
+            blocks = enemyBlocks;
+        }
+        foreach (var block in blocks)
+        {
+            block.GetComponent<Block>().SummonBlock.SetActive(false);  // 关闭高亮
+            // 由于格子只有在格子高亮时才可召唤，故关闭高亮后格子就不可作为召唤目标位置了
+        }
+    }
+
+    /// <summary>
+    /// 执行召唤(是直接更改坐标移过去了)
+    /// </summary>
+    /// <param name="_player">召唤的玩家</param>
+    /// <param name="_monster">要召唤的怪兽</param>
+    /// <param name="_block">要召唤到的格子</param>
+    public void Summon(int _player, GameObject _monster, Transform _block)
+    {
+        _monster.transform.SetParent(_block);
+        _monster.transform.localPosition = Vector3.zero;  // 这两步将_monster的坐标设为与_block一致
+
+        _monster.GetComponent<BattleCard>().state = BattleCardState.inBlock;  // 卡牌状态设为在场上（格子里）
+        _block.GetComponent<Block>().card = _monster;
+
+        SummonCounter[_player]--;  // 当前玩家的召唤次数-1
     }
 }
